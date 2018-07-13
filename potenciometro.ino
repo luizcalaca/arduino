e1e0f31348bcb63d7a691d2fbf3b0009b9f4ddfa
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2018-07-13 18:00:28 -0300

    Controle do brilho de um LED ligado à porta 13 por meio de um potenciômetro
    Controle do brilho de um LED ligado à porta 13 por meio de um potenciômetro.

---
+++ potenciometro.ino
@@ -0,0 +1,14 @@
+#define potPin 0   
+#define ledPin 13  
+
+int valPot = 0; 
+
+void setup() { 
+pinMode(ledPin,OUTPUT);
+} 
+
+void loop() {    
+valPot =  analogRead(potPin); 
+valPot = map(valPot,0,1023,0,255); 
+analogWrite(ledPin,valPot ); 
+}
