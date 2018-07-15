c1e4e68ca939d5d0edc6f13a2d23822e851a18ff
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2018-07-15 20:22:42 -0300

    Adição do controle da luz
    Adição do controle da luz. Quando o sensor percebe que a luz ambiente está desligada, então, liga um led que está no pino PWM 13 no Arduino.

--- sensor_luminosidade_led_serial.ino
+++ sensor_luminosidade_led_serial.ino
@@ -1,9 +1,14 @@
 void setup() {
  Serial.begin(9600); //inicia a comunicação com o computador
+ pinMode(13,OUTPUT);
 }
 
 void loop() {
   int luz = analogRead(5); //faz a leitura da luminosidade por meio da porta analógica A5
   Serial.println(luz); //enviar o valor da luminosidade, por meio da porta serial, para o computador
+  if( luz == 1023)
+    digitalWrite(13,HIGH);
+  else
+    digitalWrite(13,LOW);
   delay(500);
 }
